211cd599e9360c13cdb7ef78c7c06c4a03ed2c09
Shaitan84 <o.seleznev1984@gmail.com>
2020-10-24 12:08:29 +0300

    fix HW

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,8 +2,9 @@
 const searchForm = document.querySelector('.search-form');
 const galleryWrap = document.querySelector('.gallery');
 const loadMore = document.querySelector('.btn-load-more');
-const btnSearch = document.querySelector('.btn')
+const btnSearch = document.querySelector('.btn');
+const loadMoreBtnRef = document.querySelector('[data-action="load-more"]');
 
 export default {
-	searchForm, galleryWrap, loadMore, btnSearch
+	searchForm, galleryWrap, loadMore, btnSearch, loadMoreBtnRef
 }
--- src/js/render-gallery.js
+++ src/js/render-gallery.js
@@ -2,23 +2,17 @@
 import galleryTemp from '../tamplates/galleryItems.hbs'
 import refs from './refs.js'
 import createModal from './modalBox.js';
+import loadMoreBtn from './load-more-buton.js';
 
 function updateGallery(pics) {
 	refs.galleryWrap.innerHTML = "";
 	const markup = galleryTemp(pics);
 	refs.galleryWrap.insertAdjacentHTML('beforeend', markup);
 	clearSerch();
-// 	window.scrollTo({
-//   top: 100,
-//   left: 100,
-//   behavior: 'smooth'
-// });
 };
 
 function clearSerch() {  
     refs.searchForm.query.value = '';   
-    // headerFormRef.innerHTML = ''; 
-    // refs.galleryWrap.innerHTML = ''; 
 }
 
 function loadMorePics(pics) {
